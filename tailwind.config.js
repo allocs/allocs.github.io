5944ca335dd1856230dc1cb253fba8100d945524
Alex Jans <ajans@wisc.edu>
2025-01-25 11:31:17 -0600

    still fixing background 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ export default {
       'pydjpink':'#BC4F56'
     },
     backgroundImage: {
-      'homepage': "url('/images/backgroundLightGray.png')",
+      'homepage': "url('/assets/images/backgroundLightGray.png')",
     },
     extend: {
       fontFamily: {
