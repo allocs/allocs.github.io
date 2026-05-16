4651255c74e9765f3243019bb9d0c6366f4e01eb
Alex Jans <ajans@wisc.edu>
2026-05-16 00:31:13 -0500

    Made portfolio mobile friendly

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,122 +8,191 @@ import Artwork1 from '../assets/images/Bella Cartoon.png';
 import Artwork2 from '../assets/images/Self portrait.png';
 import Artwork3 from '../assets/images/Pydjum Valentines less texture.png';
 import kalecabbagebrocolli from '../assets/images/Kale, Cabbage, Brocolli.jpg';
+import waitingRoom from '../assets/images/publicDefenderWeWillBeRightBack.png';
+import wispdIntro from '../assets/images/wispd_intro.gif'
 import { Rnd } from 'react-rnd';
 
+//calculate the starting x and ys as well of the various things
+//im gonna calculate a cluster center (marked by the label, not really quite a center) based on the screen width, and the elements will base their x/y on that
+//an ipad pro's width is 1024. That seems like a good cut off
+const portfolioCoords = [15, 15];
+const videoCoords = [19,200];
+const artCoords = [19,1100];
+const videoDims = [384,216, .8];
+const spacing = [0,225];
+if (screen.width > 1024){
+    portfolioCoords[0] = screen.width*(0.35);
+    portfolioCoords[1] = 15;
+    videoCoords[0] = 10;
+    videoCoords[1] = 100;
+    artCoords[0] = screen.width*.4;
+    artCoords[1] = 98;
+    videoDims[0] = 480;
+    videoDims[1] = 270;
+    videoDims[2] = 1;
+    spacing[0] = screen.width*.2;
+    spacing[1] = 135;
+}
 
 const Portfolio = () => {
   return (
-    <div className="flex h-full bg-cork bg-fixed bg-repeat justify-center">
-        <Rnd
-            default={{
-            x: 500,
-            y: 30,
-            width: 300,
-            height: 180,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+    <div className="flex h-full bg-cork bg-fixed bg-repeat">
 
-        >
-            <img
-                className='h-full w-full pixel'
-                src={portfolioHeader}
-                alt='Index card that says "Portfolio"'
-            /> 
-        </Rnd>
-        <Rnd
-            default={{
-            x: 100,
-            y: 50,
-            width: 200,
-            height: 200,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+            <Rnd
+                className='col-span-2'
+                default={{
+                x: portfolioCoords[0],
+                y: portfolioCoords[1],
+                width: 300,
+                height: 180,
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
 
-        >
-            <img
-                className='h-full w-full pixel'
-                src={videosHeader}
-                alt='Sticky note that says "Videos"'
-            /> 
-        </Rnd>
-        <Rnd
-            default={{
-            x: 900,
-            y: 50,
-            width: 200,
-            height: 200,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+            >
+                <img
+                    className='h-full w-full pixel'
+                    src={portfolioHeader}
+                    alt='Index card that says "Portfolio"'
+                    draggable='false'
+                /> 
+            </Rnd>
+            <Rnd
+                default={{
+                x: videoCoords[0],
+                y: videoCoords[1],
+                width: 200,
+                height: 200,
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
 
-        >
-            <img
-                className='h-full w-full pixel'
-                src={artHeader}
-                alt='Sticky note that says "art"'
-            /> 
-        </Rnd>
-        <Rnd
-            default={{
-            x: 50,
-            y: 250,
-            width: 320,
-            height: 240,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+            >
+                <img
+                    className='h-full w-full pixel'
+                    src={videosHeader}
+                    alt='Sticky note that says "Videos"'
+                    draggable='false'
+                /> 
+            </Rnd>
+            <Rnd
+                default={{
+                x: artCoords[0] + spacing[0],
+                y: artCoords[1],
+                width: 200,
+                height: 200,
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
 
-        >
-            <iframe className='h-full w-full'  src="https://www.youtube.com/embed/6tet8p_UsPA?si=QVSXStb9lp-oNwIu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-            </iframe>
-        </Rnd>
-        <Rnd
-            default={{
-            x: 80,
-            y: 560,
-            width: 320,
-            height: 240,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+            >
+                <img
+                    className='h-full w-full pixel'
+                    src={artHeader}
+                    alt='Sticky note that says "art"'
+                    draggable='false'
+                /> 
+            </Rnd>
+            <Rnd
+                default={{
+                x: videoCoords[0] - 5 + spacing[0],
+                y: videoCoords[1] + spacing[1],
+                width: videoDims[0],
+                height: videoDims[1],
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
 
-        >
-            <iframe className='h-full w-full'  src="https://www.youtube.com/embed/HXku_5jckAM?si=lsKfOMFmcBNaTF6n" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-            </iframe>
-        </Rnd>
-        <Rnd
-            default={{
-            x: 430,
-            y: 360,
-            width: 320,
-            height: 240,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+            >   
+                <iframe className='h-full w-full p-4'  src="https://www.youtube.com/embed/6tet8p_UsPA?si=QVSXStb9lp-oNwIu" title="Growth" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                </iframe>
+            </Rnd>
+            <Rnd
+                default={{
+                x: videoCoords[0] - 10,
+                y: videoCoords[1] + (spacing[1]*2),
+                width: videoDims[0],
+                height: videoDims[1],
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
+                className='justify-center'
 
-        >
-            <iframe className='h-full w-full'  src="https://www.youtube.com/embed/1d7fDApiG3M?si=1fYZRXktJoncHKCL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-            </iframe>
-        </Rnd>
-        <Rnd
-            default={{
-            x: 850,
-            y: 360,
-            width: 476,
-            height: 378,
-            }}
-            lockAspectRatio={true}
-            minWidth={100}
+            >
 
-        >
-            <img
-                className='h-full w-full'
-                src={kalecabbagebrocolli}
-                alt='A still life of kale, cabbage, and brocolli'
-            /> 
-        </Rnd>
+                <iframe className='h-full w-full p-4'  src="https://www.youtube.com/embed/HXku_5jckAM?si=lsKfOMFmcBNaTF6n" title="Bella's family" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                </iframe>
+            </Rnd>
+            <Rnd
+                default={{
+                x: videoCoords[0] +3 + spacing[0],
+                y: videoCoords[1] + (spacing[1]*3),
+                width: videoDims[0],
+                height: videoDims[1],
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
+
+            >
+
+                <iframe className='h-full w-full p-4'  src="https://www.youtube.com/embed/1d7fDApiG3M?si=1fYZRXktJoncHKCL" title="Between the Lines Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                </iframe>
+            </Rnd>
+                        < Rnd
+                default={{
+                x: artCoords[0] ,
+                y: artCoords[1] + spacing[1],
+                width: videoDims[0],
+                height: videoDims[1],
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
+
+            >
+                <img
+                    className='h-full w-full'
+                    src={waitingRoom}
+                    alt='Art made for the Wisconsin State Public Defenders Office depicting Gideon, Lady Liberty, and Lady Justice in a waiting room.'
+                    draggable='false'
+                /> 
+            </Rnd>
+            <Rnd
+                default={{
+                x: artCoords[0] - 10 + spacing[0],
+                y: artCoords[1] + spacing[1]*2,
+                width: 476 * videoDims[2],
+                height: 378 * videoDims[2],
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
+
+            >
+                <img
+                    className='h-full w-full'
+                    src={kalecabbagebrocolli}
+                    alt='A still life of kale, cabbage, and brocolli'
+                    draggable='false'
+                /> 
+            </Rnd>
+            < Rnd
+                default={{
+                x: artCoords[0] + 2 ,
+                y: artCoords[1] + spacing[1]*3 + 120,
+                width: videoDims[0],
+                height: videoDims[1],
+                }}
+                lockAspectRatio={true}
+                minWidth={100}
+
+            >
+                <img
+                    className='h-full w-full pixel'
+                    src={wispdIntro}
+                    alt='Gif of a simple animation made for the Wisconsin State Public Defenders Office using their logo.'
+                    draggable='false'
+                /> 
+            </Rnd>
+            <div className='h-[2200px] md:h-full'></div>
     </div>
   );
     <div className='flex flex-col'>
