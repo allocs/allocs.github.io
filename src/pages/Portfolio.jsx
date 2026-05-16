1c0bb0e0be8595de936262487226d83affd47767
Alex Jans <ajans@wisc.edu>
2026-05-16 00:51:08 -0500

    more adjusting

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -38,9 +38,9 @@ if (screen.width > 1024){
         portfolioCoords[1] = 15;
         videoCoords[0] = 10;
         videoCoords[1] = 100;
-        artCoords[0] = screen.width*.4;
+        artCoords[0] = screen.width*.5;
         artCoords[1] = 98;
-        spacing[0] = screen.width*.2;
+        spacing[0] = screen.width*.3;
         spacing[1] = 100;
     }
 }
